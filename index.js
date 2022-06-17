7dbe4f1ebade92bd6f866c5ad3eed04a993b7cd5
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-17 18:28:56 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversed = word.split('').reverse().join('');
+  return reversed === word;
+function isPalindrome(word) {
+  let reversed = word.split('').reverse().join('');
+  return reversed === word;
 }
-
+}
 /* 
   Add your pseudocode here
 */
+//in this lab you input a string but the feedback is in boolean form
+//-convert strings to lowercase
+//-reverse the string
+//-compare  the original string to the reverse done
+//-return true if they are the same, false if not
 
 /*
   Add written explanation of your solution here
 */
+//-convert the string to lowercase.
+//-reverse the string, 
+//-and compare the reversed string to the original string
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
